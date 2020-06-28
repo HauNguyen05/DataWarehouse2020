a2b25bb1246a49ba92fedb66ee464792d50438b5
Hau Nguyen <Hau Nguyen@192.168.1.10>
2020-06-28 15:52:07 +0700

    download file process success, insert table log

--- src/Component_1/CheckFileName.java
+++ src/Component_1/CheckFileName.java
@@ -1,48 +1,59 @@
 package Component_1;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CheckFileName {
-	private String nameGroup;
 	private String typeFile;
-	private String nameFile;
-	private boolean isZip;
+	private String isUnzip;
 
-	public ArrayList<String> information() {
-		ArrayList<String> result = new ArrayList<String>();
+	public Map<String, String> information() {
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("typeFile", this.typeFile);
+		result.put("isUnzip", this.isUnzip);
 		return result;
 
 	}
 
-	static boolean checkFileName(String fileName, String listSyntax) {
+	boolean checkFileName(String fileName, String listSyntax) {
 		String[] name = fileName.split("\\.");
 		String[] syntaxs = listSyntax.split(",");
+		boolean result = false;
 		if (name.length > 1) {
 			fileName = fileName.trim();
 			for (String syntax : syntaxs) {
 				syntax = syntax.trim();
 				if (fileName.startsWith(syntax.trim())) {
 					String nameGroup = fileName.substring(syntax.length());
-					return checkNameGroup(nameGroup);
+					result = checkNameGroup(nameGroup);
+					if (result) {
+						information();
+						return true;
+					}
 				}
 			}
 		}
 		return false;
 	}
 
-	static boolean checkNameGroup(String name) {
+	boolean checkNameGroup(String name) {
 		String[] split = name.split("\\.");
-		System.out.println("so " + split[0]);
-		if (parseInt(split[0]) && typeFile(split[1])) {
+		if (parseInt(split[0]) && checkTypeFile(split[1])) {
 			return true;
 		}
 		return false;
 	}
 
-	static boolean typeFile(String nameType) {
+	public boolean checkTypeFile(String nameType) {
 		String[] type = { "docx", "doc", "xlsx", "csv", "txt", "zip", "rar" };
 		for (String string : type) {
 			if (nameType.equals(string)) {
+				this.typeFile = string;
+				if (nameType.equals("zip") || nameType.equals("rar")) {
+					this.isUnzip = "1";
+				} else {
+					this.isUnzip = "0";
+				}
 				return true;
 			}
 		}
@@ -57,10 +68,10 @@ public class CheckFileName {
 			return false;
 		}
 	}
-
 	public static void main(String[] args) {
-		String syntaxs = "sinhvien_sang_nhom, sinhvien_chieu_nhom";
-		String fileName = "sinhvien_sang_nhom06.xlsx";
-		System.out.println(checkFileName(fileName, syntaxs));
+		CheckFileName c = new CheckFileName();
+		boolean a =c.checkFileName("sinhvien_chieu_nhom11.xlsx", "sinhvien_sang_nhom, sinhvien_chieu_nhom");
+		System.out.println(a);
+		System.out.println(c.information().get("typeFile"));
 	}
 }
--- src/Component_1/DownloadFile.java
+++ src/Component_1/DownloadFile.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
 
 import com.chilkatsoft.CkGlobal;
 import com.chilkatsoft.CkScp;
@@ -21,6 +22,7 @@ public class DownloadFile {
 	private int port;
 	private static CkSsh ssh;
 	private Connection connectionControl;
+	private CheckFileName check;
 	static {
 		try {
 			System.loadLibrary("chilkat");
@@ -33,16 +35,17 @@ public class DownloadFile {
 	public DownloadFile() {
 		try {
 			connectionControl = ConnectDB.getConectionControl("root", "");
+			check = new CheckFileName();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
 	/*
-	 * kết nối table config lấy các field thông tin kết nối server
+	 * connect table config to get information
 	 */
 	public void setup(String nameConfig) throws SQLException {
-		String sql = "SELECT server_src,user_src, pwd_src, port_src, path_dir_src, destination, syntax_file_name from data_config where id ="
+		String sql = "SELECT server_src,user_src, pwd_src, port_src, path_remote, path_dir_src, syntax_file_name from data_config where id ="
 				+ nameConfig;
 		PreparedStatement statement = connectionControl.prepareStatement(sql);
 		ResultSet r = statement.executeQuery();
@@ -70,38 +73,52 @@ public class DownloadFile {
 	}
 
 	public String[] getListFileName() {
-		// Mở kênh để gửi lệnh qua
+		// open channel to excute cmd
 		int channelNum = ssh.OpenSessionChannel();
-		// Nếu mở kênh bị lỗi thì hiện lỗi
+		// if fail print fail
 		if (channelNum < 0) {
-			System.out.println("Lỗi mở kênh");
+			System.out.println("open channel failure");
 			System.exit(0);
 		}
-		// Câu lệnh list ra tất cả tên file có trong folder data
+		// cmd list all file in folder
 		String cmd = "cd " + remotePath + "; ls";
 		ssh.SendReqExec(channelNum, cmd);
-		// Gọi Channel để đọc đầu ra cho đến khi nhận được lệnh đóng kênh của máy chủ.
 		ssh.ChannelReceiveToClose(channelNum);
-		// Lưu kết quả
 		String cmdResult = ssh.getReceivedText(channelNum, "ansi");
-		System.out.println(cmdResult);
 		String[] listFileNames = cmdResult.split("\n");
+		System.out.println(listFileNames.length);
 		return listFileNames;
 	}
 
-	public void downloadFileProcess() throws SQLException {
-		setup("1");
+	public void downloadFileProcess(String idConfig) throws SQLException {
+		setup(idConfig);
 		connectServer();
 		String[] listFileNames = getListFileName();
 		for (String fileName : listFileNames) {
-			boolean isDownload = CheckFileName.checkFileName(fileName, syntaxFileName);
+			boolean isDownload = check.checkFileName(fileName, syntaxFileName);
 			if (isDownload) {
-				boolean a = downloading(fileName);
-				System.out.println("download file " + fileName +" " +a );
+				boolean downloaded = downloading(fileName);
+				if (downloaded) {
+					Map<String, String> infor = check.information();
+					infor.put("fileName", fileName);
+					insertLogTable(idConfig, infor);
+					System.out.println("insert log success");
+				}
 			}
 
 		}
 		ssh.Disconnect();
+	}
+
+	public void insertLogTable(String idConfig, Map<String, String> infor) throws SQLException {
+		String update = "INSERT INTO data_config_log(id, file_name,file_type, status, unzip) VALUES (?, ?,?,?, ?)";
+		PreparedStatement statement = connectionControl.prepareStatement(update);
+		statement.setString(1, idConfig);
+		statement.setString(2, infor.get("fileName"));
+		statement.setString(3, infor.get("typeFile"));
+		statement.setString(4, "ER");
+		statement.setString(5, infor.get("isUnzip"));
+		statement.execute();
 	}
 
 	public boolean downloading(String fileName) {
@@ -111,13 +128,13 @@ public class DownloadFile {
 			System.out.println(scp.lastErrorText());
 			return false;
 		}
-		success = scp.DownloadFile(remotePath+fileName, destinationPath+fileName);
-		
+		success = scp.DownloadFile(remotePath + fileName, destinationPath + fileName);
+
 		return success;
 	}
 
 	public static void main(String[] args) throws SQLException {
 		DownloadFile c = new DownloadFile();
-		c.downloadFileProcess();
+		c.downloadFileProcess("1");
 	}
 }
