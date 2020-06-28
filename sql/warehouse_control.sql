582f48d75bd49a4e27b285fb0379079c764456ab
thong144145 <thongmap0909310872@gmail.com>
2020-06-28 18:24:27 +0700

    # WARNING: head commit changed in the meantime
    Update table controll and code load data to staging

--- sql/warehouse_control.sql
+++ sql/warehouse_control.sql
@@ -16,7 +16,7 @@
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
+use warehouse_control;
 -- ----------------------------
 -- Table structure for data_config
 -- ----------------------------
@@ -45,7 +45,7 @@ CREATE TABLE `data_config`  (
 -- ----------------------------
 -- Records of data_config
 -- ----------------------------
-INSERT INTO `data_config` VALUES (1, 'drive.ecepvn.org', '2227', 'guest_access', '123456', '/volume1/ECEP/song.nguyen/DW_2020/data/', 'D:/DataWarehouse2020/data/source/', 'jdbc:mysql://localhost:3306/', 'com.mysql.jdbc.Driver', 'warehouse_extra_db', 'root', '0985153812', 'data', '11', 'STT,MSSV,Họ Lót ,Tên SV,Ngày sinh,Mã Lớp,Lớp,Số điện thoại,Email,Quê quán,Ghi Chú', 'E:\\warehouse2020\\logs\\log_config_3.txt', 'sinhvien_sang_nhom, sinhvien_chieu_nhom');
+INSERT INTO `data_config` VALUES (1, 'drive.ecepvn.org', '2227', 'guest_access', '123456', '/volume1/ECEP/song.nguyen/DW_2020/data/', 'D://DataWarehouse2020//data//source', 'jdbc:mysql://localhost:3306/', 'com.mysql.jdbc.Driver', 'warehouse_extra_db', 'root', '0985153812', 'data', '11', 'STT,MSSV,Họ Lót ,Tên SV,Ngày sinh,Mã Lớp,Lớp,Số điện thoại,Email,Quê quán,Ghi Chú', 'log_config_3.txt', 'sinhvien_sang_nhom, sinhvien_chieu_nhom');
 
 -- ----------------------------
 -- Table structure for data_config_log
@@ -66,7 +66,8 @@ CREATE TABLE `data_config_log`  (
 -- ----------------------------
 -- Records of data_config_log
 -- ----------------------------
-INSERT INTO `data_config_log` VALUES (1, 'sinhvien_chieu_nhom11.xlsx', 'ER', 'xlsx', 0, NULL, NULL);
+
+/*INSERT INTO `data_config_log` VALUES (1, 'sinhvien_chieu_nhom11.xlsx', 'ER', 'xlsx', 0, NULL, NULL);
 INSERT INTO `data_config_log` VALUES (1, 'sinhvien_chieu_nhom14.xlsx', 'ER', 'xlsx', 0, NULL, NULL);
 INSERT INTO `data_config_log` VALUES (1, 'sinhvien_chieu_nhom15.xlsx', 'ER', 'xlsx', 0, NULL, NULL);
 INSERT INTO `data_config_log` VALUES (1, 'sinhvien_chieu_nhom16.xlsx', 'ER', 'xlsx', 0, NULL, NULL);
@@ -89,6 +90,24 @@ INSERT INTO `data_config_log` VALUES (1, 'sinhvien_sang_nhom2.xlsx', 'ER', 'xlsx
 INSERT INTO `data_config_log` VALUES (1, 'sinhvien_sang_nhom4.xlsx', 'ER', 'xlsx', 0, NULL, NULL);
 INSERT INTO `data_config_log` VALUES (1, 'sinhvien_sang_nhom7.xlsx', 'ER', 'xlsx', 0, NULL, NULL);
 INSERT INTO `data_config_log` VALUES (1, 'sinhvien_sang_nhom8.txt', 'ER', 'txt', 0, NULL, NULL);
-INSERT INTO `data_config_log` VALUES (1, 'sinhvien_sang_nhom9.xlsx', 'ER', 'xlsx', 0, NULL, NULL);
-
+INSERT INTO `data_config_log` VALUES (1, 'sinhvien_sang_nhom9.xlsx', 'ER', 'xlsx', 0, NULL, NULL);*/
+INSERT INTO `data_config_log` VALUES (1,'sinhvien_chieu_nhom11.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_chieu_nhom14.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_chieu_nhom15.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_chieu_nhom16.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_chieu_nhom2.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_chieu_nhom3.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_chieu_nhom4.csv','ER','csv',0,1,','),
+(1,'sinhvien_chieu_nhom4.txt','ER','xlsx',0,0,NULL),
+(1,'sinhvien_chieu_nhom4.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_chieu_nhom5.zip','ER','xlsx',1,0,NULL),
+(1,'sinhvien_chieu_nhom6.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_sang_nhom06.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_sang_nhom1.txt','ER','txt',0,0,'\t'),
+(1,'sinhvien_sang_nhom1.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_sang_nhom11.xlsx','ER','xlsx',0,0,NULL),(1,'sinhvien_sang_nhom13.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_sang_nhom14.xlsx','ER','xlsx',0,0,NULL),(1,'sinhvien_sang_nhom15.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_sang_nhom16.xlsx','ER','xlsx',0,0,NULL),(1,'sinhvien_sang_nhom2.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_sang_nhom4.xlsx','ER','xlsx',0,0,NULL),(1,'sinhvien_sang_nhom7.xlsx','ER','xlsx',0,0,NULL),
+(1,'sinhvien_sang_nhom8.txt','ER','txt',0,1,'|'),(1,'sinhvien_sang_nhom9.xlsx','ER','xlsx',0,0,NULL);
 SET FOREIGN_KEY_CHECKS = 1;
