875f1c80922cef545a1f89c5ff7c2cdae9df52cc
Coders.Mickey <thuongnguyen.it78@gmail.com>
2020-07-26 14:32:43 +0700

    Edit method transfrom data

--- src/component_3/DataWarehouse.java
+++ src/component_3/DataWarehouse.java
@@ -30,15 +30,16 @@ public class DataWarehouse {
 	final String TRANSFORM_FAIL = "TF"; // transform
 
 	/**
-	 * 1. Connect tới db_control 
-	 * 2. Lấy các thuộc tính cần thiết add vào list
+	 * 1. Connect tới db_control 2. Lấy các thuộc tính cần thiết add vào list
 	 * dataControl
 	 */
 	public void connectDataControl() throws SQLException {
 		CONNECTION_CONTROL = ConnectDB.getConectionControl("root", PASSWORD);
 		Statement statementControl = CONNECTION_CONTROL.createStatement();
-		String sql = "select databasse, user_des, pwd_des, table_name_des, column_number, column_name,"
-				+ "dbwarehouse_name, dbwarehouse_user, dbwarehouse_password, dbwarehouse_table from data_config limit 1;";
+		String sql = "select databasse, user_des, pwd_des, table_name_des,"
+				+ "column_number, column_name,dbwarehouse_name, dbwarehouse_user,"
+				+ "dbwarehouse_password, dbwarehouse_table from data_config limit 1;";
+
 		ResultSet rsControl = statementControl.executeQuery(sql);
 
 		dataControl = new ArrayList<String>();
@@ -62,18 +63,30 @@ public class DataWarehouse {
 
 	/**
 	 * 1. Connect tới db_staging nhờ từ những thuộc tính có trong list data_Control
-	 * 2. Lấy toán bộ record trong db_staging add vào data_Staging
+	 * 2. Lấy toán bộ record trong db_staging add vào data_Staging 3. Nếu trường nào
+	 * rỗng thì gán giá trị là "NULL"
 	 */
 	public void connectDataStaging() throws SQLException {
 
 		CONNECTION_STAGING = ConnectDB.getConnection(dataControl.get(0), dataControl.get(1), dataControl.get(2));
+
 		Statement statementStaging = CONNECTION_STAGING.createStatement();
+
 		ResultSet rsStaging = statementStaging.executeQuery("select * from " + dataControl.get(3));
+
 		dataStaging = new ArrayList<String>();
 
 		while (rsStaging.next()) {
 			String temp = "";
-			for (int i = 2; i < Integer.parseInt(dataControl.get(4)) + 2; i++) { // column_number
+			for (int i = 1; i < Integer.parseInt(dataControl.get(4)) + 1; i++) { // column_number
+				String a = rsStaging.getString(i);
+				
+				if (rsStaging.getString(i).equalsIgnoreCase("\r") 
+					|| rsStaging.getString(i).equalsIgnoreCase("")) {
+					temp += "," + "NULL";
+					continue;
+				}
+				
 				temp += "," + rsStaging.getString(i);
 			}
 			temp = temp.substring(1);
@@ -86,8 +99,7 @@ public class DataWarehouse {
 
 	/**
 	 * 1. Connect tới db_warehouse nhờ từ những thuộc tính có trong list
-	 * data_Control 
-	 * 2. Lấy toàn bộ record trong db_warehouse add vào list
+	 * data_Control 2. Lấy toàn bộ record trong db_warehouse add vào list
 	 * data_Warehouse
 	 */
 	public void connectDataWarehouse() throws SQLException {
@@ -113,58 +125,59 @@ public class DataWarehouse {
 	}
 
 	public void handleData() throws SQLException {
-		
-		//Chạy lần lượt từng phần tử trong dataStaging
+
+		// Chạy lần lượt từng phần tử trong dataStaging
 		for (String dataIndex : dataStaging) {
-			
+
 			switch (checkData(dataIndex)) {
-			//1. Nếu trùng thì nhảy sang record tiếp theo
+			// 1. Nếu trùng hoàn toàn thì nhảy sang record tiếp theo
 			case 1:
 				break;
-			//2. Nếu nó trùng các key update lại trường expired sau đó thì addData();
+			// 2. Nếu nó trùng MSSV thì update lại SV trong dt_warehouse và addData();
 			case 2:
-				updateTableWareHouse(dataIndex);
 				addData(dataIndex);
 				break;
-			//3. Nếu có ít nhất một field bị rỗng thì edit lại thành "NULL" sau đó addData();
-			case 3:
-				editFieldWareHouse(dataIndex);
-				addData(dataIndex);
-				break;
-			//4. Nếu bình thường thì addData();
+			// 0. Nếu bình thường thì addData();
 			default:
 				addData(dataIndex);
 				break;
 			}
-					
-			
+
 		}
-		
-		
+
 	}
 
 	private void setExpireDate(String id) throws SQLException {
-//		String sql = "UPDATE " + tableSDW + " SET date_expire = CURDATE(), id_file = " + currIDFile
-//				+ "  WHERE id_student = " + id;
-//		Statement st = connDW.createStatement();
-//		st.execute(sql);
+		String sql = "UPDATE " + dataControl.get(9) + " SET dt_expired = CURDATE()" + " WHERE SK_SV = " + id;
+
+		Statement statementWarehouse = CONNECTION_WAREHOUSE.createStatement();
+		int rsWarehouse = statementWarehouse.executeUpdate(sql);
 
 	}
 
-//	private void executeSuccess() throws SQLException {
-//		// update status file trong log thành TS
-//		updateStatus(TRANSFORM_SUCCESS);
-//		// truncate Staging
-//		truncateStaging();
-//	}
+	private void executeSuccess() throws SQLException {
+		// Update status file trong log thành TS
+		updateStatus(TRANSFORM_SUCCESS);
+		
+		// Truncate Staging
+		truncateStaging();
+	}
+
+	private void updateStatus(String status) throws SQLException {
+		String sql = "UPDATE " + "data_config_log" +" SET status =" + status +"WHERE status = TF limit 1";
+		Statement statementControl = CONNECTION_CONTROL.createStatement();
+		ResultSet rsControl = statementControl.executeQuery(sql);
+	}
 
 	private void truncateStaging() throws SQLException {
-//		Statement state = connControll.createStatement();
-//		// lấy thông tin data warehouse từ config
-//		ResultSet rs = state.executeQuery("SELECT database_name FROM config_database WHERE config_database.stt = 1");
-//		rs.next();
-//		Statement stateS = connStaging.createStatement();
-//		stateS.execute("TRUNCATE " + rs.getString(0) + "." + tableSDW);
+		String sql = "TRUNCATE TABLE " + dataControl.get(3);
+
+		Statement statementStaging = CONNECTION_STAGING.createStatement();
+		ResultSet rsStaging = statementStaging.executeQuery(sql);
+	}
+	
+	private void checkStatus() {
+		
 	}
 
 	private void addData(String dataIndex) throws SQLException {
@@ -179,8 +192,10 @@ public class DataWarehouse {
 
 		value = value.substring(1);
 		// CHU Y
-		String sql = "INSERT INTO " + dataControl.get(10)
-				+ " (MSSV, HoLot, Ten, NgaySinh, MaLop, TenLop, DienThoai, Email, QueQuan, GhiChu)" + " VALUES(" + value
+		String sql = "INSERT INTO " + dataControl.get(9)
+				+ " (STT, MSSV, HoLot, Ten, NgaySinh, MaLop, TenLop,"
+				+ " DienThoai, Email, QueQuan, GhiChu)" 
+				+ " VALUES(" + value
 				+ ");";
 		Statement statementWarehouse = CONNECTION_WAREHOUSE.createStatement();
 		int rows = statementWarehouse.executeUpdate(sql);
@@ -188,41 +203,24 @@ public class DataWarehouse {
 
 	}
 
-	private void editFieldWareHouse(String dataIndex) {
-
-	}
-
-	private void updateTableWareHouse(String dataIndex) {
-
-	}
-
-	private int checkData(String dataIndex) {
+	private int checkData(String dataIndex) throws SQLException {
 		// 1. Giống hoàn toàn
 		if (dataWarehouse.contains(dataIndex)) {
 			return 1;
 		}
-		// 2. Giống trường khóa chính (MSSV hoặc số điện thoại hoặc email)
+		// 2. Giống trường khóa chính (MSSV)
 		String arrDataIndex[] = dataIndex.split(",");
-
+		
+		int i = 0;
 		for (String dtWareHouse : dataWarehouse) {
+			i += 1;
 			String arrWarehouse[] = dtWareHouse.split(",");
-
-			if (arrDataIndex[0].equalsIgnoreCase(arrWarehouse[0]) || arrDataIndex[6].equalsIgnoreCase(arrWarehouse[6])
-					|| arrDataIndex[7].equalsIgnoreCase(arrWarehouse[7])) {
-
+			if (arrDataIndex[1].equals(arrWarehouse[1])) {			
+				setExpireDate(Integer.toString(i));
 				return 2;
 			}
 
 		}
-		// 3. Nếu có field là NULL
-
-		for (int i = 0; i < arrDataIndex.length; i++) {
-
-			if (arrDataIndex[i].equals(""))
-				return 3;
-
-		}
-
 		return 0;
 	}
 
